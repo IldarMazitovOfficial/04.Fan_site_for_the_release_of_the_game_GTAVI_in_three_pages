08e54c39ee161d5903aedbaaaa1621b21a32b092
IldarMazitov <djdar92@mail.ru>
2025-04-18 07:13:57 +0500

    Значительные изменения в файле maincontent3 и изменение формата картинок в .png

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Header from './components/Header.jsx'
 import './index.css'
 
 const root = document.getElementById('root')
